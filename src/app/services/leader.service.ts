f1ef3dd8c3a22be89581bf227bef3068ae086bf1
Elka74100 <luc.kaing2@gmail.com>
2019-09-25 18:01:58 +0200

    Assignement 4 - Task 2

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -5,35 +5,49 @@ import { LEADERS } from '../shared/leaders';
 import { delay } from 'rxjs/operators';
 import { Observable, of } from 'rxjs';
 
+import { map, catchError } from 'rxjs/operators';
+
+import { ProcessHTTPMsgService } from './process-httpmsg.service';
+
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { baseURL } from '../shared/baseurl';
+
 @Injectable({
   providedIn: 'root'
 })
 export class LeaderService {
 
-  constructor() { }
+  // constructor() { }
 
-  // getLeaders(): Promise<Leader[]> {
-  //   return Promise.resolve(LEADERS);
+  // getLeaders(): Observable<Leader[]> {
+  //   return of(LEADERS).pipe(delay(2000));
   // }
 
-  // getLeader(id: string): Promise<Leader> {
-  //   return Promise.resolve(LEADERS.filter((leader) => (leader.id === id))[0]);
+  // getLeader(id: string): Observable<Leader> {
+  //   return of(LEADERS.filter((leader) => (leader.id === id))[0]).pipe(delay(2000));
   // }
 
-  // getFeaturedLeader(): Promise<Leader> {
-  //   return Promise.resolve(LEADERS.filter((leader) => leader.featured)[0]);
+  // getFeaturedLeader(): Observable<Leader> {
+  //   return of(LEADERS.filter((leader) => leader.featured)[0]).pipe(delay(2000));
   // }
+
+  constructor(private http: HttpClient,
+    private processHTTPMsgService: ProcessHTTPMsgService,
+    ) { }
 
   getLeaders(): Observable<Leader[]> {
-    return of(LEADERS).pipe(delay(2000));
+    return this.http.get<Leader[]>(baseURL + 'leadership')
+      .pipe(catchError(this.processHTTPMsgService.handleError));
   }
 
   getLeader(id: string): Observable<Leader> {
-    return of(LEADERS.filter((leader) => (leader.id === id))[0]).pipe(delay(2000));
+    return this.http.get<Leader>(baseURL + 'leadership/' + id)
+      .pipe(catchError(this.processHTTPMsgService.handleError));
   }
 
   getFeaturedLeader(): Observable<Leader> {
-    return of(LEADERS.filter((leader) => leader.featured)[0]).pipe(delay(2000));
+    return this.http.get<Leader[]>(baseURL + 'leadership?featured=true').pipe(map(leaders => leaders[0]))
+      .pipe(catchError(this.processHTTPMsgService.handleError));
   }
 
 }
